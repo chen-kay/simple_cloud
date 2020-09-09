14dc760bed94d335cfd0bd335fc7ac425184bf5b
chensl <l1328076914@gmail.com>
2020-09-09 08:53:48 +0800

    fix 项目启动不外呼

--- cloud/fs/thread/__init__.py
+++ cloud/fs/thread/__init__.py
@@ -12,7 +12,7 @@ logger = logging.getLogger('logs')
 
 
 class FsThread(threading.Thread):
-    queue_list = {}
+    # queue_list = {}
     queue = Queue
     status = Status
     monitor = monitor
@@ -30,21 +30,22 @@ class FsThread(threading.Thread):
             sleep(1)
 
     def start_queue_thread(self, domain, project_id):
-        queue_name = '{0}_{1}'.format(domain, project_id)
-        if queue_name in self.queue_list:
-            return False
+        # queue_name = '{0}_{1}'.format(domain, project_id)
+        # if queue_name in self.queue_list:
+        #     return False
         _thread = self.queue(domain, project_id)
-        self.queue_list[queue_name] = _thread
+        # self.queue_list[queue_name] = _thread
         _thread.start()
         return True
 
     def stop_queue_thread(self, domain, project_id):
-        queue_name = '{0}_{1}'.format(domain, project_id)
-        _thread = self.queue_list.get(queue_name, None)
-        if _thread:
-            _thread.stop()
-            del _thread
-            self.queue_list.pop(queue_name)
+        pass
+        # queue_name = '{0}_{1}'.format(domain, project_id)
+        # _thread = self.queue_list.get(queue_name, None)
+        # if _thread:
+        #     _thread.stop()
+        #     del _thread
+        #     self.queue_list.pop(queue_name)
 
     def start_status_thead(self):
         '''启动呼叫状态线程
